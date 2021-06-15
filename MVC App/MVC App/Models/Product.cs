d20aa21a00edc8da02642ac4baf749fc3b561f20
yashshivhare911119@gmail.com <yashshivhare911119@gmail.com>
2021-06-15 09:17:48 +0530

    third

---
+++ MVC App/MVC App/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace MVC_App.Models
+{
+    public partial class Product
+    {
+        public Product()
+        {
+            Cart = new HashSet<Cart>();
+            Categories = new HashSet<Categories>();
+            Feedback = new HashSet<Feedback>();
+        }
+
+        public string ProId { get; set; }
+        public string ProName { get; set; }
+        public string ProDescription { get; set; }
+        public decimal? ProPrice { get; set; }
+        public int? Stock { get; set; }
+
+        public virtual ICollection<Cart> Cart { get; set; }
+        public virtual ICollection<Categories> Categories { get; set; }
+        public virtual ICollection<Feedback> Feedback { get; set; }
+    }
+}
---
+++ MVC App/MVC App/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace MVC_App.Models
+{
+    public partial class Users
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserAddress { get; set; }
+        public string UserMobile { get; set; }
+
+        public virtual CustomerInfo CustomerInfo { get; set; }
+        public virtual UserLogin UserLogin { get; set; }
+    }
+}
